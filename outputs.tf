496eaec10fd5b464b785492a15bbd6f9aee484fd
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:02 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,32 @@ output "api_management_workspace_api_version_sets" {
   description = "All api_management_workspace_api_version_set resources"
   value       = azurerm_api_management_workspace_api_version_set.api_management_workspace_api_version_sets
 }
+output "api_management_workspace_api_version_sets_api_management_workspace_id" {
+  description = "List of api_management_workspace_id values across all api_management_workspace_api_version_sets"
+  value       = [for k, v in azurerm_api_management_workspace_api_version_set.api_management_workspace_api_version_sets : v.api_management_workspace_id]
+}
+output "api_management_workspace_api_version_sets_description" {
+  description = "List of description values across all api_management_workspace_api_version_sets"
+  value       = [for k, v in azurerm_api_management_workspace_api_version_set.api_management_workspace_api_version_sets : v.description]
+}
+output "api_management_workspace_api_version_sets_display_name" {
+  description = "List of display_name values across all api_management_workspace_api_version_sets"
+  value       = [for k, v in azurerm_api_management_workspace_api_version_set.api_management_workspace_api_version_sets : v.display_name]
+}
+output "api_management_workspace_api_version_sets_name" {
+  description = "List of name values across all api_management_workspace_api_version_sets"
+  value       = [for k, v in azurerm_api_management_workspace_api_version_set.api_management_workspace_api_version_sets : v.name]
+}
+output "api_management_workspace_api_version_sets_version_header_name" {
+  description = "List of version_header_name values across all api_management_workspace_api_version_sets"
+  value       = [for k, v in azurerm_api_management_workspace_api_version_set.api_management_workspace_api_version_sets : v.version_header_name]
+}
+output "api_management_workspace_api_version_sets_version_query_name" {
+  description = "List of version_query_name values across all api_management_workspace_api_version_sets"
+  value       = [for k, v in azurerm_api_management_workspace_api_version_set.api_management_workspace_api_version_sets : v.version_query_name]
+}
+output "api_management_workspace_api_version_sets_versioning_scheme" {
+  description = "List of versioning_scheme values across all api_management_workspace_api_version_sets"
+  value       = [for k, v in azurerm_api_management_workspace_api_version_set.api_management_workspace_api_version_sets : v.versioning_scheme]
+}
 
